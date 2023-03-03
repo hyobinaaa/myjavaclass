8dc6225456639b0efc8f67a2d7b0eda4d1a09d51
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-03 17:18:03 +0900

    Create Math.java

---
+++ Javaclasstest/Math.java
@@ -0,0 +1,24 @@
+package Javaclasstest;
+
+public class Math {
+    int add(int x, int y){
+        return x+y;
+    }
+
+    int square(int i){
+        return i * i;
+
+
+    }
+
+    double square(double i){
+        return i * i;
+    }
+    public static void main(String[] args) {
+        Math obj = new Math();
+        System.out.println(obj.square(10));
+        System.out.println(obj.square(3.14));
+        
+    }
+    
+}
