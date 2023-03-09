79ffd8a422fc4e28ba83e6ac42fed996c6b80ed3
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-09 12:49:03 +0900

    Create StringTest3.java

---
+++ Javaclasstest/StringTest3.java
@@ -0,0 +1,12 @@
+package Javaclasstest;
+
+public class StringTest3 {
+    public static void main(String[] args) {
+        String subject = "Money"; 
+        String other = "has no value if is not used"; // Money has no value if is not used
+        String sentence = subject + other;
+
+        System.out.println(sentence);
+    }
+    
+}
