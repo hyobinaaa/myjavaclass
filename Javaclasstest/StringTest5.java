0a566299f5a8cc43345bc4a5d5e854f7de0f7374
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-09 12:48:59 +0900

    Create StringTest5.java

---
+++ Javaclasstest/StringTest5.java
@@ -0,0 +1,11 @@
+package Javaclasstest;
+
+public class StringTest5 {
+    public static void main(String[] args) {
+        int i = Integer.parseInt("123");
+        double d = Double.parseDouble("3.141592");
+
+        System.out.println(i + d);
+    }
+    
+}
