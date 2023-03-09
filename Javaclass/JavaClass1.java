469623010719eed5fc4442c34ff60d0bf8047703
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-09 12:38:24 +0900

    Update JavaClass1.java

--- Javaclass/JavaClass1.java
+++ Javaclass/JavaClass1.java
@@ -2,8 +2,7 @@ package Javaclass;
 
 public class JavaClass1 {
     public static void main(String[] args) {
-       // string 객체는 imutable입니다. mutable이 아닙니다.
-       // 이말은 string 객체 값으로 변경할수 없다.
+      
     }
     
 }
