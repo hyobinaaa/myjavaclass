b99b06fef31ce273ae5b86e712abe258f40cbf4f
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-09 12:49:01 +0900

    Create StringTest4.java

---
+++ Javaclasstest/StringTest4.java
@@ -0,0 +1,12 @@
+package Javaclasstest;
+
+public class StringTest4 {
+    public static void main(String[] args) {
+        int x = 20;
+        System.out.println("결과값은" + x ); // 결과값 20 
+        String answer = "The answer is" + 100; //  The answer is 100
+
+        System.out.println(answer);
+    }
+    
+}
