b4ac9bb82cd370818642616a0490eb34235bbce3
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-09 12:10:13 +0900

    Update StringTest2.java

--- Javaclasstest/StringTest2.java
+++ Javaclasstest/StringTest2.java
@@ -1,4 +1,4 @@
-package Javaclass;
+package Javaclasstest;
 
 import java.util.Scanner;
 
@@ -14,9 +14,9 @@ public class StringTest2 {
             if (str.equals("quit") == true)
                             break;
             if (str.matches("^www\\.(+)")){
-                System.out.println(str + "은 'www'로 시작합니다.");
+                System.out.println(str + "은 'www'로 시작합니다."); // 만약에 이러한 문자열을  다음과 같이입력하면 출력
             } else   {
-                System.out.println(str + "은 'www' 로 시작하지 않습니다.");
+                System.out.println(str + "은 'www' 로 시작하지 않습니다."); // 예시 문자열을 입력하면  다음과 같이 출력
             }
             }
         }
