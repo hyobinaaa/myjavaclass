4abccf34dec2c76c702dafeae6e9fcc956c6ac2c
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-13 19:47:57 +0900

    Create MyCounterTest1.java

---
+++ Javaclasstest/MyCounterTest1.java
@@ -0,0 +1,12 @@
+package Javaclasstest;
+
+public class MyCounterTest1 {
+    public static void main(String[] args) {
+        MyCounter obj  = new MyCounter();
+        int x = 10;
+        obj.inc(x);
+        System.out.println("x=" + x);
+        
+    }
+    
+}
