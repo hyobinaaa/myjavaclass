daab460e8181a040d3cd97d0b55fd8ad96833a0f
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-13 19:48:04 +0900

    Create Person.java

---
+++ Javaclasstest/Person.java
@@ -0,0 +1,10 @@
+package Javaclasstest;
+
+public class Person {
+    public static void main(String[] args) {
+        StringClass1 p = new StringClass1();
+        p.name = "홍길동";
+        p.age = 23;
+    }
+    
+}
