603f2c9b926916f60b22eecfd44cd46c814cceb7
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-15 19:36:06 +0900

    Create Test.java

---
+++ Javaclasstest/Test.java
@@ -0,0 +1,14 @@
+package Javaclasstest;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] a = new int [10]; 
+        System.out.println(a[0]);
+        
+        for (int i = 0; i <= 10 ; i++){
+            a[i] = i + 1;
+        }
+
+    }
+    
+}
