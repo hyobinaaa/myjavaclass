afbcaa81a4a33ed11bc2397ccbbb25ec91d2161f
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-16 12:49:53 +0900

    Create Point.java

---
+++ Javaclasstest/Point.java
@@ -0,0 +1,13 @@
+package Javaclasstest;
+
+public class Point {
+    public int x = 0;
+    public int y = 0;
+
+    // 생성자 
+    public Point(int x , int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+}
