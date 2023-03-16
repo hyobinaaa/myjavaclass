3b7bc0b1b1377fad421b9483cea0a59b43ad0dde
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-16 12:00:57 +0900

    Create SportsCars.java

---
+++ Javaclasstest/SportsCars.java
@@ -0,0 +1,8 @@
+package Javaclasstest;
+
+public class SportsCars {
+    int speed; // 속도 , 상속을 위해 private 안붙음
+    public void setSpeed(int speed) { // 속도 변경 메서드
+        this.speed = speed;
+    }
+}
