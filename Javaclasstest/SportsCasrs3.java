df1278445b1d7ae0c515e3aec9dc4372313700dd
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-16 12:00:46 +0900

    Create SportsCasrs3.java

---
+++ Javaclasstest/SportsCasrs3.java
@@ -0,0 +1,12 @@
+package Javaclasstest;
+
+public class SportsCasrs3 {
+    public static void main(String[] args) {
+        SportsCars2 obj = new SportsCars2();
+
+        obj.speed = 10;
+        obj.setSpeed(60);
+        obj.setTurbo(true);
+    }
+    
+}
