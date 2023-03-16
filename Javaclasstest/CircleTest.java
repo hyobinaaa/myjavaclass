a0b5b1e3796b8a54eae4bf741dc403d50311cf1a
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-16 12:50:00 +0900

    Create CircleTest.java

---
+++ Javaclasstest/CircleTest.java
@@ -0,0 +1,40 @@
+package Javaclasstest;
+
+class Point {
+    private int x;
+    private int y;
+
+    public Point (int x ,int y){
+        this.x = x;
+        this.y = y;
+    }
+    public String toString(){
+        return "Point [x= " +x +", y = "+y +"]";
+    }
+}
+
+class Circle{
+    private int radius;
+    private Point center;
+
+    public Circle(Point p , int r){
+        center = p;
+        radius = r;
+
+    }
+    public String toString(){
+        return "Circle [radius = " + radius + " , center ="+ center +" ]";
+    }
+}
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Point p = new Point(25, 78);
+
+        Circle c = new Circle(p, 10);
+
+        System.out.println(c);
+        
+    }
+    
+}
