0deb49b4b0f9c1ade57ee82030a17af4419ab1a6
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-22 23:39:04 +0900

    Create GameCharacterTest.java

---
+++ Javaclasstest/GameCharacterTest.java
@@ -0,0 +1,25 @@
+package Javaclasstest;
+
+import java.util.ArrayList;
+
+class GameCharacter {
+    private class GameItem{
+        String name;
+        int type;
+        int price;
+
+
+        int getPrice() {return price;}
+
+        public String toString(){
+            return "GameItem [name = "+ name + " ,type ="+type+" , price=" + price +
+              "]";
+
+        private ArrayList<GameItem>list = new ArrayList<GameItem>()
+        }
+    }
+}
+
+public class GameCharacterTest {
+    
+}
