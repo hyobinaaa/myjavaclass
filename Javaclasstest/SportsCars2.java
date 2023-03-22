544221b2ff1735390d41fefefb75abc0fee30187
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-22 23:38:48 +0900

    Update SportsCars2.java

--- Javaclasstest/SportsCars2.java
+++ Javaclasstest/SportsCars2.java
@@ -1,7 +1,9 @@
 package Javaclasstest;
 
+
 public class SportsCars2 extends SportsCars {
     boolean turbo; // 추가된 필드
+
 
     public void setTurbo(boolean flag) { // 터보 모드 설정 메서드
         turbo = flag;
