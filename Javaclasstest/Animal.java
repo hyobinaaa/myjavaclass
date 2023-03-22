9d879737f5edba27472f8f11a4761c04ddcead88
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-22 23:39:18 +0900

    Create Animal.java

---
+++ Javaclasstest/Animal.java
@@ -0,0 +1,10 @@
+package Javaclasstest;
+
+public class Animal {
+    private  double weight;
+    String  picture;
+
+    void eat(){System.out.println("eat()가 호출되었습니다.");}
+    void sleep(){System.out.println("sleep()가 호출되었습니다");}
+    
+}
