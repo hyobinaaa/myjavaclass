03b49bf7d6e64de26ed2463d2852a3fae7289465
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-22 23:52:17 +0900

    Update GameCharacterTest.java

--- Javaclasstest/GameCharacterTest.java
+++ Javaclasstest/GameCharacterTest.java
@@ -15,11 +15,36 @@ class GameCharacter {
             return "GameItem [name = "+ name + " ,type ="+type+" , price=" + price +
               "]";
 
-        private ArrayList<GameItem>list = new ArrayList<GameItem>()
+        
+
+         }
         }
+        private ArrayList<GameItem>list = new ArrayList<GameItem>();
+
+        public void add(String name, int type , int price ){
+            GameItem item = new GameItem();
+            item.name = name;
+            item.type = type;
+            item.price = price;
+            list.add(item);
     }
+
+        public void print(){
+            int total = 0;
+            for(GameItem item : list){
+                System.out.println(item);
+                total +=item.getPrice();
+            }
+            System.out.println(total);
+        }
 }
 
 public class GameCharacterTest {
+    public static void main(String[] args) {
+        GameCharacter charac = new GameCharacter();
+        charac.add("Sword", 1 ,100);
+        charac.add("Gun" , 2 , 50);
+        charac.print();
+    }
     
 }
