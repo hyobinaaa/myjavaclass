758016be58d3f8d8d5608e71ee4dec54271752a9
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-23 11:53:45 +0900

    Update SportsCasrs3.java

--- Javaclasstest/SportsCasrs3.java
+++ Javaclasstest/SportsCasrs3.java
@@ -2,11 +2,12 @@ package Javaclasstest;
 
 public class SportsCasrs3 {
     public static void main(String[] args) {
-        SportsCars2 obj = new SportsCars2();
+        SportsCars2 obj = new SportsCars2(); // 자식 클래스 객체 생성
 
+        // 부모 클래스 필드와 메소드 사용
         obj.speed = 10;
         obj.setSpeed(60);
-        obj.setTurbo(true);
+        obj.setTurbo(true); // 자체 메소드 사용
 
         
     }
