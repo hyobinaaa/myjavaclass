7e2a1f0de38c37f34c9641019bde21b1301ebbb7
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-03-31 17:02:58 +0900

    Create ShapeTesr3.java

---
+++ Javaclasstest/ShapeTesr3.java
@@ -0,0 +1,20 @@
+package Javaclasstest;
+
+public class ShapeTesr3 {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+
+        shapes[0] = new rectangle();
+        shapes[1] = new Triangle();
+        shapes[2] = new Circle();
+
+
+        drawShapes(shapes);
+    }
+
+    public static void drawShapes(Shape[] arr){
+        for(int i =0 ; i < arr.length; i++)
+        arr[i].draw();
+    }
+    
+}
