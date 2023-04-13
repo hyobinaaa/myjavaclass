df15248a4d68bbbb48403673dd0a1d4d91176add
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-13 12:18:32 +0900

    Update Car.java

--- ObjectClass/Car.java
+++ ObjectClass/Car.java
@@ -19,10 +19,15 @@ public class Car {
     @Override
     public boolean equals(Object obj){ 
        Car objCar = (Car) obj;
+       // obj는 car 클래스의 객체라 가정한다
+       // 그래야, Car 클래스의 인스턴스의 brand , model 맴버에 접근할 수 있다
+       // 하향 형변환
        
-       if( (this.brand).equals(objCar.brand) && (this.model).equals(objCar.model))
-            return true;
-        return false;
+       if( (this.brand).equals(objCar.brand) && (this.model).equals(objCar.model)){
+         
+        return true;
+       }
+     return false;
     }
     
     
