8ff0a734128b841da818ae2ba4b514982c85cde4
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-13 11:52:28 +0900

    Create CarTest.java

---
+++ ObjectClass/CarTest.java
@@ -0,0 +1,18 @@
+package ObjectClass;
+
+public class CarTest {
+    public static void main(String[] args) {
+        // Car obj = new Car();
+        // Class classvar = car.getClass();
+        // System.out.println(classvar.getName());
+
+
+        // System.out.println("브랜드:" + car.brand + " ," + "모델:" + car.model);
+
+        Car car1 = new Car("현대" , "그랜저");
+        Car car2 = new Car("현대" , "그랜저");
+        System.out.println(car1.equals(car2));
+        System.out.println(car1 == car2);
+        
+    }
+}
