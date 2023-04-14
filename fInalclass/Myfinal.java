d29ab72e251b4c50cc7a5a6c1600d8090f60a8e0
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-14 16:43:29 +0900

    Create Myfinal.java

---
+++ fInalclass/Myfinal.java
@@ -0,0 +1,10 @@
+package fInalclass;
+
+// final keyword로 이 클래스를 종단 클래스로 지정
+// 중단 클래스는 상속될수 없다.
+public class Myfinal {
+    public void test(){
+        System.out.println("TEST");
+    }
+    
+}
