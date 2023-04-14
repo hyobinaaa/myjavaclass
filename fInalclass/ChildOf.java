a3c871327d8d5900a1562d40197d456f4ad6dc4f
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-14 16:43:42 +0900

    Create ChildOf.java

---
+++ fInalclass/ChildOf.java
@@ -0,0 +1,11 @@
+package fInalclass;
+
+public class ChildOf extends FinalTest {
+
+    // 부모(조상) 클래스의 종단 메소드는 오버라이딩할 수 없다.
+    @Override
+    public String getFirstPlayer(){
+        return "WHITE";
+    }
+    
+}
