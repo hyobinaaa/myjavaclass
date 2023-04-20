b32c3f78e9f1bd56aa2721a369e2f2281d54f373
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-20 19:51:50 +0900

    Create Televsion.java

---
+++ Interface/Televsion.java
@@ -0,0 +1,15 @@
+package Interface;
+
+public class Televsion implements RemoteControl {
+    public void turnOn()
+    {
+        // 실제로 tv 전원을 켜기 위한 코드가 들어 간다
+    }
+    public void turnOff(){
+    // 실제로 tv 전원을 끄기 위한 코드가 들어 간다
+    }
+
+   
+}
+
+
