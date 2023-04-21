bb83c51157a877717a04f800362e234f7db19167
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-21 17:11:28 +0900

    Update Student.java

--- InterfaceTest/Student.java
+++ InterfaceTest/Student.java
@@ -4,31 +4,42 @@ package InterfaceTest;
 // 구현해야 한다.
 
 public class Student implements Comparable{
-    private String name;
-    private String dept;
-    public String getName(){
-        return name;
-    }
+private String name;
+private String dept;
 
-    
+Student(String name, String dept) {
+  this.name = name;
+  this.dept = dept;
+}
+public String getName() {
+  return name;
+}
+public void setName(String name) {
+  this.name = name;
+}
 
-    @Override
-    public int compareTo (Comparable obj) {
-        // null인 경우는 없다고 가정
-        // interface는 class 처럼 데이터 타입으로 사용될 수 있다
-        // 특정 interface 타입의 변수는 그 interface를 구현하는
-        // 모든 클래스(와 그 클래스의 모든 후손 클래스)의 객체를 담을 수 있다
-        Student std = (Student)obj;
-        // String 클래스는 java.lang 패키지의 Comparable 인터페이스를
-        // 이미 구현하고 있다. 즉, 그 인터페이스의 추상 메소드인
-        // compareTo 메소드를 구현하고 있다.
-        // String의 compareTo 메소드는 비교 대상보다
-        // 사전 순서상 앞에 위치하면 -1, 같으면 0 , 뒤에 위치하면 1을 반환한다. 
-        return name.compareTo(std.getName());
-        
-    
-        
-    }
+public String getDept() {
+  return dept;
+}
 
+public void setDept(String dept) {
+  this.dept = dept;
+}
+
+@Override
+public int compareTo(Comparable obj) {
+  // null 인경우는 없다고 가정
+  // interface는 class처럼 데이터 타입으로 사용될 수 있다.
+  // 특정 interface 타입의 변수는 그 interface를 구현하는
+  // 모든 클래스(와 그 클래스의 모든 후손)의 객체를 담을 수 있다.
+
+  Student std = (Student) obj;
+  // String 클래스는 java.lang 패키지의 Comparable 인터페이스를 이미 구현하고 있다.
+  // 즉, 그 인터페이스의 추상메소드인
+  // compareTo 메소드를 구현하고 있다.
+  // String의 compareTo 메소드는 비교 대상보다
+  // 사전 순서상 앞에 위치하면 -1, 같으면 0 뒤에 위차하면 1을 반환한다
    
+  return name.compareTo(std.getName()); 
+}
 }
