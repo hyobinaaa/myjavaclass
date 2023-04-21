4379902cefa052cce5b5f3e0542048d2212c97ed
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-21 17:11:25 +0900

    Update Test.java

--- InterfaceTest/Test.java
+++ InterfaceTest/Test.java
@@ -6,13 +6,20 @@ public class Test {
         String s2 = "BBC";
         System.out.println(s1.compareTo(s2));
 
-        Student std1 = new Student();
-        Student std2 = new Student();
-        findlarger(std1, std2);
+        // Student std1 = new Student();
+        // Student std2 = new Student();
+        // findlarger(std1, std2);
+        Car c1 = new Car("브랜드1", "모델1", 1, 2000);
+        Car c2 = new Car("브랜드2", "모델2", 1, 1000);
+        Car c3 = new Car();
+        System.out.println(c3); // [브래든:Unknown , 모델: Unknown , speed:0 , price:0]
+
+        findLarger(c1, c2);
+
     }
 
 
-    public static void findlarger(Comparable c1 , Comparable C2){
+    public static void findLarger(Comparable c1 , Comparable C2){
         if(c1.compareTo(C2) == 0){
             System.out.println("같다");
         }else if(c1.compareTo(C2) >= 0){
