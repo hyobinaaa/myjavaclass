370582c8ef3255666ae51f7f6e7cf557b8a6e61c
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-27 12:22:08 +0900

    Update RemoteControl.java

--- Interface/RemoteControl.java
+++ Interface/RemoteControl.java
@@ -4,6 +4,12 @@ public interface RemoteControl {
     public void turnOn(); // 가전제품을 켠다
     public void turnOff(); // 가전제품을 끊다
 
-    default void volumControl(int amount){};
+    default void volumControl(int amount){
+        String st = null;
+        st = amount > 0 ? "높입니다." : "줄입니다";
+        amount = Math.abs(amount); // |number|
+        System.out.println("볼륨을" + amount + "만큼" + st);
+    }
+     
      
 }
