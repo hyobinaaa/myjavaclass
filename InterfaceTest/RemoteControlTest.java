457705f1344f40c3a3a498668d4de29e286c2fa4
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-27 12:22:02 +0900

    Update RemoteControlTest.java

--- InterfaceTest/RemoteControlTest.java
+++ InterfaceTest/RemoteControlTest.java
@@ -6,9 +6,11 @@ public class RemoteControlTest {
     public static void main(String[] args) {
         RemoteControl rm = new SamsungTV();
         rm.turnOn();
+        rm.volumControl(3);
 
         rm = new LgTv();
         rm.turnOn();
+        rm.volumControl(-2);
     }
     
 }
