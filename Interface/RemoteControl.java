cc6bd30a0975fb849897a52a54169fb598e05f8d
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-27 12:15:05 +0900

    Update RemoteControl.java

--- Interface/RemoteControl.java
+++ Interface/RemoteControl.java
@@ -3,5 +3,7 @@ package Interface;
 public interface RemoteControl {
     public void turnOn(); // 가전제품을 켠다
     public void turnOff(); // 가전제품을 끊다
+
+    default void volumControl(int amount){};
      
 }
