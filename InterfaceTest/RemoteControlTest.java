b7aa7955214ee7d8a98500587d02d03da739286a
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-04-27 12:38:10 +0900

    Update RemoteControlTest.java

--- InterfaceTest/RemoteControlTest.java
+++ InterfaceTest/RemoteControlTest.java
@@ -4,13 +4,28 @@ import Interface.RemoteControl;
 
 public class RemoteControlTest {
     public static void main(String[] args) {
+        // RemoteControl rm = new SamsungTV();
+        // rm.turnOn();
+        // rm.volumControl(3);
+
+        // rm = new LgTv();
+        // rm.turnOn();
+        // rm.volumControl(-2);
+
         RemoteControl rm = new SamsungTV();
-        rm.turnOn();
-        rm.volumControl(3);
+        turnOnAndControlVolume(rm, 3);
 
         rm = new LgTv();
+        turnOnAndControlVolume(rm,-2);
+    }
+
+    public static void turnOnAndControlVolume(RemoteControl rm , int vol){
         rm.turnOn();
-        rm.volumControl(-2);
+        if(rm instanceof AdvencedRemoteControl){
+            ((AdvencedRemoteControl)rm).volumControl(vol);
+        }else{
+            System.out.println("이 기기는 원격 볼륨제어를 지원하지 않습니다");
+        }
     }
     
 }
