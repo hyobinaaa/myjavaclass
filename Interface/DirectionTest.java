eb04988fba754eede2140d49e6cd1c28d2ba34f0
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-05-04 12:25:33 +0900

    Update DirectionTest.java

--- Interface/DirectionTest.java
+++ Interface/DirectionTest.java
@@ -1,12 +1,7 @@
 package Interface;
 
 
-public interface MyConstant{
-    int NORTH = 1;
-    int EAST = 2;
-    int SOUTH = 3;
-    int WEST = 4;
-}
+
 public class DirectionTest implements MyConstant {
     public static void main(String[] args) {
         System.out.println("NORTH:" + MyConstant.NORTH);
