638a591fb0aa887b32430f571fc4fb226cb01451
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-05-12 17:50:22 +0900

    Update CallBackTest2.java

--- Anonymous/CallBackTest2.java
+++ Anonymous/CallBackTest2.java
@@ -10,7 +10,7 @@ public class CallBackTest2 {
     public static void main(String[] args) {
         CallBackTest2 cb = new CallBackTest2();
 
-        // 무명 클래스로 remotecontrol 객체를 인자로 전달하시오
+        // 무명 클래스로 RemoteControl 객체를 인자로 전달하시오
         // 1. 변수 사용해서 전달해보고 
         // 2. 변수 사용하지 않고 전달해보고.
         // 화면에는 파나소닉 테레비가 켜졌습니다
