5e5b44371f9fbfb8c3e13333f03bc014ac6fa91d
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-05-12 17:49:58 +0900

    Create CallBackTest2.java

---
+++ Anonymous/CallBackTest2.java
@@ -0,0 +1,20 @@
+package Anonymous;
+
+import Interface.RemoteControl;
+
+public class CallBackTest2 {
+    public void remoteControl(RemoteControl rc){
+        rc.turnOn();
+    }
+
+    public static void main(String[] args) {
+        CallBackTest2 cb = new CallBackTest2();
+
+        // 무명 클래스로 remotecontrol 객체를 인자로 전달하시오
+        // 1. 변수 사용해서 전달해보고 
+        // 2. 변수 사용하지 않고 전달해보고.
+        // 화면에는 파나소닉 테레비가 켜졌습니다
+        cb.remoteControl(null);
+    }
+    
+}
