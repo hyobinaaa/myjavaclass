e1dbd030ca2c6c1f19dcccbd8c38035e4aafa439
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-06-01 12:31:37 +0900

    Update Person.java

--- Lambda/Person.java
+++ Lambda/Person.java
@@ -1,12 +1,13 @@
 package Lambda;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
     private String name;
     private LocalDate birthDate;
     private String emailAddress;
-    private String gender;
+    private Sex gender;
 
 public Person(String name, LocalDate birDate, String emailAddress, Sex gender){
     this.name = name;
@@ -30,8 +31,19 @@ public String getGender(){
     return gender;
 }
 
+public int getAge() {
+//    Period p = Period.between(birthDate, LocalDate.now());
+//    return p.getYears();
+   
+   // method chaining  : 메소드 호출을 쇠사슬이 연결된 것처럼, 이어서 쭉 호출하는 것
+
+   return Period.between(birthDate, LocalDate.now().getYear());
+
+
+}
+
 public void printPerson(){
-    System.out.printf("Name : %s , Email : %s, Gender : %d, BirthDate : %s" ,name, emailAddress,gender,birthDate.toString());
+    System.out.printf("Name : %s , Email : %s, Gender : %s, BirthDate : %s" ,name, emailAddress,gender.toString(),birthDate.toString());
 }
 
 
