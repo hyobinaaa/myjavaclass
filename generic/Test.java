9ddb5abb6e28f115ea80fe0a888d6bcbf54789ba
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-06-02 17:34:59 +0900

    Update Test.java

--- generic/Test.java
+++ generic/Test.java
@@ -2,17 +2,33 @@ package generic;
 
 public class Test {
     public static void main(String[] args) {
-       StrBox sb1 = new StrBox();
-       sb1.setValue("동해물과");
+    //    StrBox sb1 = new StrBox();
+    //    sb1.setValue("동해물과");
 
-       StrBox sb2 =  new StrBox();
-       sb2.setValue("백두산이");
+    //    StrBox sb2 =  new StrBox();
+    //    sb2.setValue("백두산이");
 
-       String result = sb1.gerValue() + sb2.gerValue();
-       System.out.println(result);
+    //    String result = sb1.gerValue() + sb2.gerValue();
+    //    System.out.println(result);
 
-       int idx = result.indexOf("백두산이");
-       System.out.println(result.substring(idx));
+    //    int idx = result.indexOf("백두산이");
+    //    System.out.println(result.substring(idx));
+
+    //    Box b1 = new Box();
+    //    b1.setValue(20);
+
+    //    Box b2 = new Box();
+    //    b2.setValue(30);
+
+    //   int result =((int)b1.getValue() + (int)b2.getValue());
+    //   System.out.println(result);
+
+
+    Box strBox = new Box();
+    strBox.setValue("마르고 닳도록 하느님이 보우하사 우리나라 만세!");
+    Object v = strBox.getValue();
+    int idx = ((String)v).indexOf("우리나라");
+    System.out.println(((String)v).substring(idx));
     }
     
 }
