97bc4e791ee07ec81404b457b43b3dd6cf481e4c
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-06-02 17:35:02 +0900

    Create Box.java

---
+++ generic/Box.java
@@ -0,0 +1,14 @@
+package generic;
+
+public class Box {
+    Object value;
+
+    public Object getValue(){
+        return value;
+    }
+
+    public void setValue(Object value){
+        this.value = value;
+    }
+    
+}
