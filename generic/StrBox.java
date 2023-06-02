1573adbc78f517e6e0e2a386f46419df75aa091e
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-06-02 17:25:02 +0900

    Create StrBox.java

---
+++ generic/StrBox.java
@@ -0,0 +1,15 @@
+package generic;
+
+public class StrBox {
+
+    private String value;
+
+    public String gerValue(){
+        return value;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+    
+}
