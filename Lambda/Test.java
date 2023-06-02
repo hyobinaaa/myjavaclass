509060fa69ee31ad628e9081ac316eb9906c99be
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-06-02 17:25:10 +0900

    Update Test.java

--- Lambda/Test.java
+++ Lambda/Test.java
@@ -1,11 +1,21 @@
 package Lambda;
 
+interface MyInterface {
+    void meMethod();
+}
+
+
+
 public class Test {
-    public void test(MyInterface m1){
-        m1.print("Hello World");
-    }
     public static void main(String[] args) {
-        Test t = new Test();
+        MyInterface myInterface =() -> {
+            System.out.println("Hello, World");
+        };
+
+        myInterface.meMethod();
+    }
+   
+       
         // 1. 무명 클래스를 이용해 Test 클래스의 인스턴스 메소드 test 호출하시오.
         // t.test(new MyInterface() {
         //     public void print(String s) {
@@ -15,9 +25,14 @@ public class Test {
         // 2. 람다를 이용해 Test 클래스의 인스턴스 메소드 test 호출하시오
         // 인터페이스인데 오직 하나의 추상 메소드를 가지는 인터페이스는
         // 람다로 구현 할수 있다
+        
+         
+        
 
 
-        // t.test(s)->{System.out.println(s);};
+         
     }
+
+   
     
-}
+
