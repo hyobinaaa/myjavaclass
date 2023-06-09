6dfe6782613ddac1a63a21678d4a935c2d6d5fab
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-06-09 17:44:56 +0900

    Update Test3.java

--- Lambda/Test3.java
+++ Lambda/Test3.java
@@ -16,16 +16,9 @@ public class Test3 {
         // printGender 메소드를 호출할때, checkPerson 객체를 인자로 넘겨줘야 하는데
             // 그 객체가 남자인지 여자인지 판단해서 남자이면 true를 여자이면 false를 반환한다
 
-            Person p1 = new Person("홍길동", LocalDate.of(2000, 1, 4 ), "gdhong@gmail.com" , Sex.MALE);
-            Person p2 = new Person("일지매", LocalDate.of(1999, 2, 4 ), "gmail@gmail.com" , Sex.MALE);
-            Person p3 = new Person("박문수", LocalDate.of(1997, 4, 2 ), "park22@gmail.com" , Sex.MALE);
-            Person p4 = new Person("박문수", LocalDate.of(1996, 6, 2 ), "park22@gmail.com" , Sex.MALE);
-            Person p5 = new Person("박문수", LocalDate.of(1994, 2, 2 ), "park22@gmail.com" , Sex.MALE);
+            
 
-
-
-            List<Person> list = new ArrayList<>();
-            list.add(p1); list.add(p2); list.add(p3); list.add(p4); list.add(p5);
+            
             // java에서는 유용하게 사용할 수 있는  functional interface를 제공하고 있다
             // function interface는 오직 하나의 추상 메소드를 가지는 
 
